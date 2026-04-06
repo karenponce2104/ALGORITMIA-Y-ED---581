b781f785c656531b2486b0c49b8ebfa61643aad9
karenponce2104 <karen.ponce@pucp.edu.pe>
2026-04-06 16:07:24 -0500

    Ejercicio2_Semana3

---
+++ Semana 3/Recursion/Recursion/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void triangulo(int a, int b) { //a nunca debe ser mayor que b
+    if (a>b) return;
+    for (int i=0; i<a; i++) cout << "*";
+    cout<<endl;
+    triangulo(a+1, b);
+    for (int i=0; i<a; i++) cout << "*";
+    cout<<endl;
+}
+int main() {
+    triangulo(3, 5);
+    return 0;
+}
